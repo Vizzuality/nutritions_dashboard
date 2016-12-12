b1920a09df647bf9bc018aedd4ce0cf7e2df1bc2
edbrett <e.j.a.brett@gmail.com>
2016-12-12 18:04:03 +0100

    global solidarity selector styles, mobile

--- app/assets/javascripts/views/current_burden.js
+++ app/assets/javascripts/views/current_burden.js
@@ -42,7 +42,7 @@
           },
           bar: {
               width: {
-                  ratio: 0.7 // this makes bar width 50% of length between ticks
+                  ratio: 0.6 // this makes bar width 50% of length between ticks
               }
               // or
               //width: 100 // this makes bar width 100px
@@ -54,7 +54,9 @@
             x: {
               type: 'category',
               categories: ['Anemia', 'Non Exclusive Breasfeeding', 'Stunting', 'Wasting'],
-              tick: {},
+              tick: {
+                fit: true
+              },
               padding: {
                 left: 0,
                 right: 0
