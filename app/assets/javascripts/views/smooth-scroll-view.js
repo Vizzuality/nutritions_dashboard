af4f5ca6f36876e77241dff5fcb89463788d7d1e
edbrett <e.j.a.brett@gmail.com>
2016-12-14 13:10:19 +0100

    add smoth scroll component

---
+++ app/assets/javascripts/views/smooth-scroll-view.js
@@ -0,0 +1,30 @@
+(function(App) {
+
+  'use strict';
+
+  App.View = App.View || {};
+
+  App.View.SmoothScrollView = Backbone.View.extend({
+
+    el: '.smooth-scroll',
+
+    events: {
+      'click .js-btn-smooth-scroll' : '_smoothScroll',
+    },
+
+    _smoothScroll: function(e) {
+      var event = e.currentTarget;
+      if (location.pathname.replace(/^\//,'') == event.pathname.replace(/^\//,'') && location.hostname == event.hostname) {
+        var target = $(event.hash);
+        target = target.length ? target : $('[name=' + event.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 700);
+          return false;
+        }
+      }
+    }
+  });
+
+})(this.App);
