9903c3d79356f968901b64b6282c22717821032c
edbrett <e.j.a.brett@gmail.com>
2016-12-21 15:13:15 +0000

    set texts in regions page

--- app/assets/javascripts/views/chart_title_view.js
+++ app/assets/javascripts/views/chart_title_view.js
@@ -10,9 +10,14 @@
 
     render: function() {
       var data = this.collection.toJSON()[0];
-
+      var title;
+      if ( data.group_name === 'global' ) {
+        title = 'the world';
+      } else {
+        title = 'the ' + data.title + ' region';
+      }
       this.$el.html(this.template({
-        title: data.title
+        title: title
       }));
     }
   });
