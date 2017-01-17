ee2cecd792c0eaa314dcc01afc2bad0a8e8efe09
edbrett <e.j.a.brett@gmail.com>
2017-01-17 13:18:47 +0100

    set share modal styles

--- app/assets/javascripts/views/modals/mobile_menu_view.js
+++ app/assets/javascripts/views/modals/mobile_menu_view.js
@@ -35,9 +35,9 @@
       var hidden = !!this.status.get('hidden');
       this.$('.c-mobile-menu').toggleClass('-is-open', !hidden);
       this.$('.mobile').toggleClass('-is-open', !hidden);
-      if (this.$el.hasClass('-transparent')) {
-        this.$el.toggleClass('-transparent', hidden);
-      }
+      // if (this.$el.hasClass('-transparent')) {
+      //   this.$el.toggleClass('-transparent', hidden);
+      // }
     }
   });
 
