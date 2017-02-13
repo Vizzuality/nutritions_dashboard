8e092370b0fb62f10ab20b2be8c5c3ddba4cd09d
Clara Linos <clara.linos@gmail.com>
2017-02-13 20:32:28 +0100

    global map showing

--- app/assets/javascripts/views/texts/map_title_view.js
+++ app/assets/javascripts/views/texts/map_title_view.js
@@ -11,13 +11,19 @@
 
     render: function() {
       var data = this.collection.toJSON()[0];
-      var region = data.region ? data.region : data.income_group;
-      this.$el.find('#map').removeClass().addClass('c-map-regions ' + region );
+
       if ( this.status.get('mode') === 'global' ) {
+        this.$el.find('#map').removeClass().addClass('c-map-regions');
+
         this.$el.find('.text').html(this.template_global({
           description: data.description
         }));
       } else {
+        //Set the map
+        var region = data.region ? data.region : data.income_group;
+        this.$el.find('#map').removeClass().addClass('c-map-regions ' + region );
+
+        //Set the text
         this.$el.find('.text').html(this.template_map({
           description: data.description,
           class: data.region ? data.region : data.income_group
